1e88288a59888f786ff898a7fbb0472ea5733139
dimapashchenko1 <76619935+dimapashchenko1@users.noreply.github.com>
2021-09-22 11:56:34 +0300

    hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import ContactList from "./components/ContactLIst/ContactList";
 import { v4 as uuidv4 } from "uuid";
 import Form from "./components/Form/Form";
@@ -6,41 +6,31 @@ import Filter from "./components/Filter/Filter";
 import css from "./App.css";
 import "../node_modules/modern-normalize/modern-normalize.css";
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: uuidv4(), name: "Rosie Simpson", number: "459-12-56" },
-      { id: uuidv4(), name: "Hermione Kline", number: "443-89-12" },
-      { id: uuidv4(), name: "Eden Clements", number: "645-17-79" },
-      { id: uuidv4(), name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parseContacts = JSON.parse(contacts);
-    // console.log(this.setState);
-    if (parseContacts) {
-      this.setState({ contacts: parseContacts });
+export default function App() {
+  const [contacts, setContacts] = useState([
+    { id: uuidv4(), name: "Rosie Simpson", number: "459-12-56" },
+    { id: uuidv4(), name: "Hermione Kline", number: "443-89-12" },
+    { id: uuidv4(), name: "Eden Clements", number: "645-17-79" },
+    { id: uuidv4(), name: "Annie Copeland", number: "227-91-26" },
+  ]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const contacts = JSON.parse(localStorage.getItem("contacts"));
+    if (contacts) {
+      setContacts(contacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    const nextContacts = this.state.contacts;
-    const prevContacts = prevState.contacts;
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (nextContacts !== prevContacts) {
-      localStorage.setItem("contacts", JSON.stringify(nextContacts));
-    }
-  }
-
-  addContact = ({ name, number }) => {
-    let duplicate = this.state.contacts.find(
-      (contact) => contact.name === name
-    );
+  const addContact = (name, number) => {
+    let duplicate = contacts.find((contact) => contact.name === name);
 
     if (duplicate) {
-      alert(` Контакт ${name} уже существует!`);
+      alert(` Контакт '${name}' уже существует!`);
     } else {
       const contact = {
         id: uuidv4(),
@@ -48,47 +38,37 @@ export default class App extends Component {
         number,
       };
 
-      this.setState((prevState) => ({
-        contacts: [...prevState.contacts, contact],
-      }));
+      setContacts((prevState) => [contact, ...prevState]);
     }
   };
 
-  changeFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = (e) => {
+    setFilter(e.currentTarget.value);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = (contactId) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter(
-        (contact) => contact.id !== contactId
-      ),
-    }));
+  const deleteContact = (contactId) => {
+    setContacts((prevState) =>
+      prevState.filter((contact) => contact.id !== contactId)
+    );
   };
 
-  render() {
-    const { filter } = this.state;
-    const VisibleContacts = this.getVisibleContacts();
-
-    return (
-      <>
-        <h1 className={css}>Phonebook</h1>
-        <Form onSubmit={this.addContact} />
-        <h2 className={css}>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          contacts={VisibleContacts}
-          onDeleteContact={this.deleteContact}
-        />
-      </>
-    );
-  }
+  return (
+    <>
+      <h1 className={css}>Phonebook</h1>
+      <Form onSubmit={addContact} />
+      <h2 className={css}>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactList
+        contacts={getVisibleContacts()}
+        onDeleteContact={deleteContact}
+      />
+    </>
+  );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,55 +1,66 @@
-import { Component } from "react";
+import { useState } from "react";
 import css from "./Form.module.css";
 
-export default class Form extends Component {
-  state = { name: "", number: "" };
+export default function Form({ onSubmit }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "number":
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({ name: "", number: "" });
+    onSubmit(name, number);
+    setName("");
+    setNumber("");
   };
 
-  render() {
-    return (
-      <form className={css.form} onSubmit={this.handleSubmit}>
-        <label className={css.label}>
-          Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Steve Jobs"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            onChange={this.handleChange}
-            value={this.state.name}
-            className={css.input}
-          />
-        </label>
-        <label className={css.label}>
-          Number
-          <input
-            type="tel"
-            name="number"
-            placeholder="+38(0XX)-XXX-XX-XX"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            onChange={this.handleChange}
-            value={this.state.number}
-            className={css.input}
-          />
-        </label>
-        <button className={css.btn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          placeholder="Steve Jobs"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          onChange={handleChange}
+          value={name}
+          className={css.input}
+        />
+      </label>
+      <label className={css.label}>
+        Number
+        <input
+          type="tel"
+          name="number"
+          placeholder="+38(0XX)-XXX-XX-XX"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={handleChange}
+          value={number}
+          className={css.input}
+        />
+      </label>
+      <button className={css.btn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
